dc75ae6945210202796954fff225c1547c00020a
samina <saminanaseem@ymail.com>
2019-01-28 02:50:26 -0800

    added

---
+++ Spring/src/main/java/com/samina/spring/springboot/Application.java
@@ -0,0 +1,20 @@
+package com.samina.spring.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.samina.spring")
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+
+	}
+
+
+
+}
