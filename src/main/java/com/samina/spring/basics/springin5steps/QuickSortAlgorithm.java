1de4a011b57dbf9564a2898ebdc35391149e1ce6
samina <saminanaseem@ymail.com>
2019-01-29 14:54:54 -0800

    first commit

---
+++ src/main/java/com/samina/spring/basics/springin5steps/QuickSortAlgorithm.java
@@ -0,0 +1,11 @@
+package com.samina.spring.basics.springin5steps;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuickSortAlgorithm implements SortAlgorithm {
+	public int[] sort(int[] numbers) {
+		// Logic for Quick Sort
+		return numbers;
+	}
+}
---
+++ src/main/java/com/samina/spring/basics/springin5steps/SortAlgorithm.java
@@ -0,0 +1,5 @@
+package com.samina.spring.basics.springin5steps;
+
+public interface SortAlgorithm {
+	public int[] sort(int[] numbers);
+}
